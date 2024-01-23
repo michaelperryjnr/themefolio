e63736cfd812add7df8996a5e66340a5e61e2745
michaelperryjnr <michaelperryt97@gmail.com>
2024-01-23 16:19:58 +0000

    Added analytics

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { inject } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 
-inject();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
