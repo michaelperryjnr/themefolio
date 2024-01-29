579e0aa14081479975c3591e0df683448a404637
michaelperryjnr <michaelperryt97@gmail.com>
2024-01-29 01:03:47 +0000

    Added tailwindcss

---
+++ client/src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+export default function Header() {
+  return (
+    <>
+      <header >
+        <div>
+          <a href="/">Profile</a>
+        </div>
+        <div>
+          <a href="/">Projects</a>
+        </div>
+        <div>
+          <a href="/">Achievements</a>
+        </div>
+        <div>
+          <a href="/">Contact</a>
+        </div>
+      </header>
+    </>
+  );
+}
