a73e54711a2f0526169a1248d46d4c9bc77e4ed3
michaelperryjnr <michaelperryt97@gmail.com>
2024-01-30 03:58:16 +0000

    Added project section

---
+++ client/src/Sections/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <>
+      <section
+        className="mx-auto h-screen relative px-6 py-6 flex justfy-center items-center"
+        id="projects"
+      >
+        <h1 className="text-blue-500 text-4xl font-semibold text-center">Projects</h1>
+      </section>
+    </>
+  );
+}
