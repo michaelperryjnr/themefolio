6932aed5cd5cc27a352004ec33a66d2da436c4c0
michaelperryjnr <michaelperryt97@gmail.com>
2024-01-30 04:00:05 +0000

    Imported sections to page

---
+++ client/src/Components/Home.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Projects from "../Sections/Projects";
+import About from "../Sections/About";
+import Contact from "../Sections/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <section className="hero mx-auto h-screen relative flex-1  flex flex-col justify-center lg:justify-between px-6 py-6">
+        <div className="flex flex-row justify-between  relative max-sm:justify-center max-sm:py-8 ">
+          <a
+            className="text-blue-600 font-semibold text-3xl max-sm:text-3xl"
+            href="/"
+          >
+            TheMeFolio
+          </a>
+          <a
+            className="text-blue-600 font-semibold text-3xl text-right max-sm:[display:none]"
+            href="/"
+          >
+            Tettey <br />
+            Michael Perry
+          </a>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="w-96 h-96 item-center rounded-full border-[15px] border-blue-600 flex fle-col justify-center items-center max-sm:w-56 max-sm:h-56 px-40 py-40">
+            <div className="w-96 w h-96 item-center rounded-full border-[20px] border-blue-600 blur-lg max-sm:w-56 max-sm:h-56 px-40 py-40"></div>
+          </div>
+        </div>
+
+        <div className="max-sm:my-6">
+          <div>
+            <h2 className="text-2xl font-semibold max-sm:text-xl max-sm:text-center">
+              Web Developer & SWE
+            </h2>
+            <h3 className="text-xl max-sm:text-xl max-sm:text-center">
+              From{" "}
+              <span className="text-blue-700 font-semibold text-2xl">
+                GH
+              </span>
+            </h3>
+          </div>
+          <div className="socials"></div>
+        </div>
+      </section>
+      <About />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
