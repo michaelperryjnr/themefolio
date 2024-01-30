d1f13246ec12aea01598eb205126da48f759ac80
michaelperryjnr <michaelperryt97@gmail.com>
2024-01-30 03:57:50 +0000

    Added about section

---
+++ client/src/Sections/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <>
+      <section
+        className="mx-auto h-screen relative px-6 py-6 flex justfy-center items-center"
+        id="about"
+      >
+        <h1 className="text-blue-500 text-4xl font-semibold text-center">
+          About
+        </h1>
+      </section>
+    </>
+  );
+}
