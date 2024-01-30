57c89f86517dbe71c4cf03130288c8a962e0dd38
michaelperryjnr <michaelperryt97@gmail.com>
2024-01-30 03:58:04 +0000

    Added contact section

---
+++ client/src/Sections/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <>
+      <section
+        className="mx-auto h-screen relative px-6 py-6 flex justfy-center items-center"
+        id="contact"
+      >
+        <h1 className="text-blue-500 text-4xl font-semibold text-center">
+          Contact
+        </h1>
+      </section>
+    </>
+  );
+}
