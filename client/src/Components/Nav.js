6755ed6e408a47c361c95c6bd06ed3f9195e8dd6
michaelperryjnr <michaelperryt97@gmail.com>
2024-01-30 15:46:24 +0000

    Fixed nav bar

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,19 +1,44 @@
 import React from "react";
 
+
 export default function Nav() {
   return (
-    <div className="flex  justify-end fixed w-full inset-0 items-center max-sm:[display:none]">
-      <div className="flex gap-8 flex-col justify-center h-screen">
-        <a className="cursor-pointer text-2xl rotate-90 my-2" href="#about">
-          ABOUT
-        </a>
-        <a className="cursor-pointer text-2xl rotate-90 my-8 hover:text-red-600" href="#projects">
-          PROJECT
-        </a>
-        <a className="cursor-pointer text-2xl rotate-90 my-8" href="#contact">
-          CONTACT
-        </a>
-      </div>
-    </div>
+    <header className="flex  justify-end items-center absolute inset-0 ">
+      <ul className="fixed max-sm:[display:none] flex gap-8 flex-col justify-center h-screen z-[100]">
+        <li className="links z-[100] rotate-90 my-8">
+          <a
+            className="hover:text-blue-600 cursor-pointer text-2xl"
+            href="#home"
+          >
+            HOME
+          </a>
+        </li>
+        <li className="links z-[100] rotate-90 my-8">
+          <a
+            className="hover:text-blue-600 cursor-pointer text-2xl"
+            href="#about"
+          >
+            ABOUT
+          </a>
+        </li>
+        <li className="links z-[100] rotate-90 my-8">
+          <a
+            className="cursor-pointer text-2xl hover:text-blue-600"
+            href="#projects"
+          >
+            PROJECT
+          </a>
+        </li>
+        <li className="links z-[100] rotate-90 my-8">
+          {" "}
+          <a
+            className="hover:text-blue-600 cursor-pointer text-2xl "
+            href="#contact"
+          >
+            CONTACT
+          </a>
+        </li>
+      </ul>
+    </header>
   );
 }
