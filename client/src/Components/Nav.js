f6aa1e6147375e22caf890d144a1c956fcb96c08
michaelperryjnr <michaelperryt97@gmail.com>
2024-01-31 03:01:29 +0000

    Created mobile nav

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function Nav() {
   return (
-    <header className="flex  justify-end items-center absolute inset-0 ">
-      <ul className="fixed max-sm:[display:none] flex gap-8 flex-col justify-center h-screen z-[100]">
-        <li className="links z-[100] rotate-90 my-8">
+    <header className="flex  justify-end items-center absolute inset-0 max-sm:justify-center max-sm:hidden">
+      <ul className="fixed flex gap-8 flex-col justify-center h-screen z-[100] max-sm:items-center max-sm:bg-black max-sm:bg-opacity-80 max-sm:w-full">
+        <li className="links z-[100] max-sm:rotate-0 rotate-90 my-8">
           <a
             className="hover:text-blue-600 cursor-pointer text-2xl"
             href="#home"
@@ -13,7 +13,7 @@ export default function Nav() {
             HOME
           </a>
         </li>
-        <li className="links z-[100] rotate-90 my-8">
+        <li className="links z-[100] max-sm:rotate-0 rotate-90 my-8">
           <a
             className="hover:text-blue-600 cursor-pointer text-2xl"
             href="#about"
@@ -21,7 +21,7 @@ export default function Nav() {
             ABOUT
           </a>
         </li>
-        <li className="links z-[100] rotate-90 my-8">
+        <li className="links z-[100] max-sm:rotate-0 rotate-90 my-8">
           <a
             className="cursor-pointer text-2xl hover:text-blue-600"
             href="#projects"
@@ -29,7 +29,7 @@ export default function Nav() {
             PROJECT
           </a>
         </li>
-        <li className="links z-[100] rotate-90 my-8">
+        <li className="links z-[100] max-sm:rotate-0 rotate-90 my-8">
           {" "}
           <a
             className="hover:text-blue-600 cursor-pointer text-2xl "
