2428e8add5f8214e52d1b18760bb7a092f9fac65
michaelperryjnr <michaelperryt97@gmail.com>
2024-02-10 22:44:44 +0000

    MeFolio server script

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
+import connect from "./database/connection.js";
+import ContactModel from "./models/ContactModel.js"
+const app = express();
+
+
+/**middlewares */
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable('x-powered-by');
+
+const PORT = process.env.NODE_ENV === "production" || "local" ? 3000 : 5000;
+
+app.get("/", (req,res) => {
+    res.status(201).json('This is the default route')
+})
+
+connect()
+    .then(() => {
+        try {
+            app.listen(PORT, () => {
+                console.log("Server started sucessfully")
+            })
+        } catch (error) {
+            console.log(error.message)
+        }
+    }).catch(error => {
+        console.log(error.message)
+    })
+    
